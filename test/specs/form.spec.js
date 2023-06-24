5c3fc7377211cccacc8e28457d54869628719674
davansealine <alinedavanse@gmail.com>
2023-06-24 17:13:38 -0300

    atualizando arquivos

--- test/specs/form.spec.js
+++ test/specs/form.spec.js
@@ -1,14 +1,16 @@
 const homeScreen = require("../screens/form.screen");
 
+
+
 describe('Validar o menu Forms', () =>{
     it('Acessar o menu Forms', async () => {
         await $('~Forms').click()
         expect(await $('~text-input')).toBeDisplayed()
-
     });
 
     it('Escrever nos campos do Formulário', async () =>{
          await $('~text-input').setValue('Preenchendo os campos')
+         expect(await $('~text-input')).toBeDisplayed()
          await $('~Dropdown').click()
          const selector = 'new UiSelector().text("Appium is awesome").className("android.widget.CheckedTextView")'
          const button = await $(`android=${selector}`)
